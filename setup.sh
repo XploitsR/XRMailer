ae865add79fa7fd988ec7882f9e889e13a8fb6af
Solomon Narh <solomonnarh97062@gmail.com>
2019-04-25 12:24:44 +0000

    initial commit

--- setup.sh
+++ setup.sh
@@ -22,13 +22,12 @@
 
 path=`pwd`
 xrlog="$path/.xrlog/xrlog"
-
 if [ ! -f "$xrlog" ]
 then
-
 path=`pwd`
 pyDir=`type -P python3 &>/dev/null && echo 1 || echo 0`
-CMDS=`type -P figlet &>/dev/null && echo 1 || echo 0`
+pyfig=`type -P figlet &>/dev/null && echo 1 || echo 0`
+xtm=`type -P xterm &>/dev/null && echo 1 || echo 0`
 xrsh="$path/xrmailer"
 xrbn="$path/banner"
 xrpy="$path/xrmailer.py"
@@ -37,93 +36,72 @@ xrhtml="$path/sample.html"
 xrtxt="$path/sample.txt"
 xrem="$path/sample.csv"
 xrpng="$path/sample.png"
-
-
 start=" "
-
 sleep 2
 echo " "
 echo "[*] Setting Up...Please Wait! "
 sleep 5
-
-
 #verify commands exist
-if [ "$pyDir" -eq 0 ] && [ "$CMDS" -eq 0 ]
+if [ "$pyDir" -eq 0 ] || [ "$pyfig" -eq 0 ] || [ "$xtm" -eq 0 ]
 then	
 echo "[!] Some Commands Are Missing!"
 sleep 1
 echo "[+] Installing...."
 sleep 1
-
 #Install needed files
-apt-get install python3 python3-pyfiglet && clear
+sudo apt-get update && apt-get install xterm -y && clear
+if [ "$xtm" -eq 1 ]; then
+xterm -T "* Installing python3 and python3-pyfiglet *" -geometry 100x30 -e "sudo apt-get install python3 python3-pyfiglet"
 start=true
-
+fi
 else
 start=true
 fi
-
 sleep 2
-
 #Rechecking 
-if [ "$pyDir" -eq 0 ] && [ "$CMDS" -eq 0 ] && [ ! $start ]
+if [ "$pyDir" -eq 0 ] || [ "$pyfig" -eq 0 ] || [ "$xtm" -eq 0 ] || [ ! $start ]
 then
-
 sleep 1
 echo "[!] Something went wrong!..."
 sleep 1
 echo "[!] Please Run The Setup Again..."
 sleep 1
-
 else
-
 echo "[*] Installations Done!!!"
-
 #verifying for necessary files
 if [ ! -f "$xrsh" ] && [ ! -f "$xrpy" ] && [ ! -f "$xrbn" ] && [ ! -f "$xrtp" ] && [ ! -f "$xrhtml" ] && [ ! -f "$xrtxt" ] && [ ! -f "$xrem" ] && [ ! -f "$xrpng" ]
 then
-
 echo "[!] Some (files) are missing..."
 echo "[!] Please re-clone XRMailer..."
 sleep 1
 exit 1
-
 else
-
-chmod 755 $xrsh
-chmod 755 $xrpy
-chmod 755 $xrbn
+chmod +x $xrsh >/dev/null 2>&1
+chmod +x $xrpy >/dev/null 2>&1
+chmod +x $xrbn >/dev/null 2>&1
 sleep 1
-
 #Checking for directories
 if [ ! -d "Emails" ] && [ ! -d "Files" ] && [ ! -d "Templates" ]
 then 
-
 sleep 1
-
 #Create Needed Directories
-mkdir Emails
-mkdir Files
-mkdir Templates
-mkdir .xrlog
-touch xrlog
+mkdir Emails >/dev/null 2>&1
+mkdir Files >/dev/null 2>&1
+mkdir Templates >/dev/null 2>&1
+mkdir .xrlog >/dev/null 2>&1
+touch xrlog >/dev/null 2>&1
 echo "Setup Done" > xrlog
-
 # (mv) files to needed directories
-mv templates.py Templates/
-mv sample.html Templates/
-mv sample.txt Templates/
-mv sample.csv Emails/
-mv sample.png Files/
-mv xrlog .xrlog/
-
+mv templates.py Templates/ >/dev/null 2>&1
+mv sample.html Templates/ >/dev/null 2>&1
+mv sample.txt Templates/ >/dev/null 2>&1
+mv sample.csv Emails/ >/dev/null 2>&1
+mv sample.png Files/ >/dev/null 2>&1
+mv xrlog .xrlog/ >/dev/null 2>&1
 proceed=true
-
 fi
-
 if [ "$proceed" ]
 then
-
 sleep 1
 echo "[*] Setup Done (^_^) ";
 echo "[*] Type ./xrmailer to get started (^_^) ";
@@ -132,7 +110,6 @@ sleep 1
 fi
 fi
 fi
-
 else
 echo " "
 echo "[*] Setup Already Done"
